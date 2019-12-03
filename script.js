57b460a03fca4431793517dd7fd05df9cf9dc4b4
Steven Kim <hansomekim@gmail.com>
2019-12-03 13:58:49 -0500

    commas

--- script.js
+++ script.js
@@ -29,12 +29,13 @@ else {
         document.querySelector('#autoClickerText').innerHTML = "You have " + localStorage.getItem('autoClicker') + " auto clickers.";
     });
 }
-
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function count() {
     localStorage.setItem('counter', parseInt(localStorage.getItem('counter'), 10) + parseInt(localStorage.getItem('button'), 10));
-    document.querySelector('#counter').innerHTML = localStorage.getItem('counter');
-    document.title = "Cookies: " + localStorage.getItem('counter');
+    document.querySelector('#counter').innerHTML = numberWithCommas(localStorage.getItem('counter'));
+    document.title = "Cookies: " + numberWithCommas(localStorage.getItem('counter'));
 }
 function upgradeButton() {
     if (parseInt(localStorage.getItem('counter')) >= parseInt(localStorage.getItem('button')) * 100) {
@@ -42,7 +43,7 @@ function upgradeButton() {
         localStorage.setItem('button', parseInt(localStorage.getItem('button'), 10) + 1);
         document.querySelector('#button').innerHTML = "Upgrade for " + localStorage.getItem('button') * 100 + "!";
     }
-    document.querySelector('#counter').innerHTML = localStorage.getItem('counter');
+    document.querySelector('#counter').innerHTML = numberWithCommas(localStorage.getItem('counter'));
 }
 let autoInterval = null;
 function upgradeAutoClicker() {
@@ -53,6 +54,6 @@ function upgradeAutoClicker() {
         autoInterval && clearInterval(autoInterval);
         autoInterval = setInterval(count, 1000 / parseInt(localStorage.getItem('autoClicker'), 10));
     }
-    document.querySelector('#counter').innerHTML = localStorage.getItem('counter');
+    document.querySelector('#counter').innerHTML = numberWithCommas(localStorage.getItem('counter'));
     document.querySelector('#autoClickerText').innerHTML = "You have " + localStorage.getItem('autoClicker') + " auto clickers.";
 }
